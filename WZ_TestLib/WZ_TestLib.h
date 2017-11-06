0d48dfea593c69b27e754ab59c8ddc0cc14189ce
hwzss <1833361588@qq.com>
2017-11-06 21:23:56 +0800

    first commit

---
+++ WZ_TestLib/WZ_TestLib.h
@@ -0,0 +1,19 @@
+//
+//  WZ_TestLib.h
+//  WZ_TestLib
+//
+//  Created by qwkj on 2017/11/6.
+//  Copyright © 2017年 qwkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WZ_TestLib.
+FOUNDATION_EXPORT double WZ_TestLibVersionNumber;
+
+//! Project version string for WZ_TestLib.
+FOUNDATION_EXPORT const unsigned char WZ_TestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WZ_TestLib/PublicHeader.h>
+
+
---
+++ WZ_TestLibTests/WZ_TestLibTests.m
@@ -0,0 +1,39 @@
+//
+//  WZ_TestLibTests.m
+//  WZ_TestLibTests
+//
+//  Created by qwkj on 2017/11/6.
+//  Copyright © 2017年 qwkj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WZ_TestLibTests : XCTestCase
+
+@end
+
+@implementation WZ_TestLibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
